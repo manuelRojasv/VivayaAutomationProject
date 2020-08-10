9808a98ad19ec7264598aaf5854632362e76aa80
manuelRojasv <56967748+manuelRojasv@users.noreply.github.com>
2020-08-10 18:38:59 -0400

    Update GuidesPage.java

--- src/test/java/Page_Obj/GuidesPage.java
+++ src/test/java/Page_Obj/GuidesPage.java
@@ -11,35 +11,20 @@ public class GuidesPage {
     By press_Guides_Tab = By.cssSelector("a[href='/guides']");
     By search_Input = By.id("guidesearch-q");
     By search_Button = By.cssSelector("button.btn.btn-primary.vertical-center");
-    By Guides_Link= By.xpath("//a[contains(text(),'Guides')]");
-    By Search_Field= By.id("guidesearch-q");
-    By Search_button= By.xpath("//form[@id='guides-search-form']/div/div[3]/button");
-    By Guide_name= By.linkText("valeria fernandez");
-    By Guide_name_info=By.xpath("//h1");
-    By Page_name=By.xpath("//h1");
 
     public GuidesPage(WebDriver driver){this.driver = driver;}
 
     public void search_Guide(String guideName){
-        driver.findElement(Guides_Link).click();
-        driver.findElement(Search_Field).sendKeys(guideName);
-        driver.findElement(Search_button).click();
-        driver.findElement(Guide_name).click();
-    }
+        //WebElement GuideLink = driver.findElement(By.xpath("//*[@id=\"mainNav\"]/li[5]/a"));
+        //GuideLink.click();
+        driver.findElement(press_Guides_Tab).click();
+        driver.findElement(search_Input).sendKeys(guideName);
+        driver.findElement(search_Button).click();
 
-    public String verify_Guide_name(){
-        return driver.findElement(Guide_name_info).getText();
     }
-
-    public String Verify_PageName(){
-        return driver.findElement(Page_name).getText();
-    }
-
     public void select_Single_Guide(String guideName){
 
         driver.findElement(By.linkText(guideName)).click();
     }
-
-
-
+    
 }
