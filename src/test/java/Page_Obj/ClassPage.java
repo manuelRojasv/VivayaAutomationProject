def1a968493fa13373fdad3f5ca223a77439b22d
manuelRojasv <56967748+manuelRojasv@users.noreply.github.com>
2020-08-16 12:13:34 -0400

    Update ClassPage.java
    Add locators, and a Method to create a new Class Event that starts 5 minutes from the current Date-time

--- src/test/java/Page_Obj/ClassPage.java
+++ src/test/java/Page_Obj/ClassPage.java
@@ -11,8 +11,12 @@ import java.util.Date;
 public class ClassPage {
     WebDriver driver;
     By event_Name = By.id("eventform-name");
+    By start_Date_Field = By.cssSelector("#eventform-startdate");
+    By add_Minutes_Arrow = By.cssSelector("a[data-action='incrementMinute']");
+    By clock_Icon = By.cssSelector(".glyphicon-time");
     By date_Picker = By.xpath("//input[@id='eventform-startdate']");
     By add_Submit = By.xpath("//button[@class='btn btn-primary']");
+
 
     public ClassPage(WebDriver driver) {
         this.driver = driver;
@@ -30,8 +34,33 @@ public class ClassPage {
 
         driver.findElement(By.cssSelector("i.glyphicon.glyphicon-calendar")).click();
         driver.findElement(By.xpath("//tr[5]/td[5]")).click();
-        //td[data-date='1596067200000']
-
         driver.findElement(add_Submit).click();
+    }
+    
+     public void create_Class_Current_Date() throws InterruptedException {
+
+        driver.findElement(event_Name).sendKeys("Event to Start now");
+        Thread.sleep(3000);
+
+        WebElement iFrame = driver.findElement(By.tagName("iframe"));
+        driver.switchTo().frame(iFrame);
+        driver.findElement(By.tagName("body")).click();
+        driver.findElement(By.tagName("body")).sendKeys("This is a Description for the Event that should start now");
+        driver.switchTo().defaultContent();
+
+
+        driver.findElement(clock_Icon).click();
+        for (int i = 0; i < 5; i++){
+            driver.findElement(add_Minutes_Arrow).click();
+        }
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+        LocalDateTime now = LocalDateTime.now();
+
+        driver.findElement(start_Date_Field).sendKeys(dtf.format(now));
+        driver.findElement(By.cssSelector("#eventform-startdate")).sendKeys(Keys.RETURN);
+        driver.findElement(add_Submit).click();
+
+
     }
 }
