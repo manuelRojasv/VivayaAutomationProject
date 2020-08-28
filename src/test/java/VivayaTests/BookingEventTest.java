3ee09f6236cf7cb44184e86dd983085e0cba9381
manuelRojasv <56967748+manuelRojasv@users.noreply.github.com>
2020-08-28 08:04:41 -0400

    Update BookingEventTest.java
    Improvements to execute the test

--- src/test/java/VivayaTests/BookingEventTest.java
+++ src/test/java/VivayaTests/BookingEventTest.java
@@ -2,19 +2,14 @@ package VivayaTests;
 
 import Page_Obj.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import org.w3c.dom.html.HTMLImageElement;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -35,7 +30,7 @@ public class BookingEventTest {
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
             objHome.click_Login_Link();
             //objLogin.set_Guide_Credentials("vivaya.automation.two@gmail.com", "automation2");
-            objLogin.set_Guide_Credentials ("mmmmrojirim@gmail.com", "perrodelmal");
+            objLogin.set_Guide_Credentials ("manurex@manu.com", "perrodelmal");
 
 
         }
@@ -50,7 +45,7 @@ public class BookingEventTest {
     }
 
     @Test(enabled = true, priority = 0)
-    public void book_Workshop_Event() throws InterruptedException {
+    public void book_Event_Several_Places() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 60);
         DashboardPage objDash = new DashboardPage(driver);
         ClassPage classPage = new ClassPage(driver);
@@ -58,7 +53,7 @@ public class BookingEventTest {
         LoginPage objLogin = new LoginPage(driver);
         EventPageDetails objEventDetails = new EventPageDetails(driver);
         GuidesPage objGuides = new GuidesPage(driver);
-        GuideProfilePage objGuideProf = new GuideProfilePage(driver);
+        GuideProfilePage objGuideProfile = new GuideProfilePage(driver);
         objDash.open_Class();
         classPage.create_Class();
         Thread.sleep(3000);
@@ -66,18 +61,17 @@ public class BookingEventTest {
         objHome.click_Login_Link();
         objLogin.set_Seeker_Credentials("horcosio@gmail.com", "perrodelmal");
         Thread.sleep(3000);
-        objGuides.search_Guide("manu rove");
-        objGuides.select_Single_Guide ("manu rove");
-        objGuideProf.select_Event_from_Guide_Profile("TEST CLASS CREATION");
+        objGuides.search_Guide("manu rex");
+        objGuides.select_Single_Guide ("manu rex");
+        objGuideProfile.select_Event_from_Guide_Profile("TEST CLASS CREATION");
         objEventDetails.select_book_Button_from_Event_Detail();
+        Thread.sleep(2000);
         objEventDetails.cancel_Event();
         objHome.perform_Logout();
         objHome.click_Login_Link();
-        objLogin.set_Guide_Credentials ("mmmmrojirim@gmail.com", "perrodelmal");
+        objLogin.set_Guide_Credentials ("manurex@manu.com", "perrodelmal");
         Thread.sleep(3000);
         objDash.cancel_an_Event();
 
-        //String sucessTest = driver.findElement(By.xpath("//*[@id=\"w0-success-0\"]")).getText();
-        //Assert.assertEquals(sucessTest, "Event has been canceled.");*/
     }
 }
