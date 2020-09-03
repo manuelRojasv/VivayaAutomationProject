6f5dafed3d7a88a4deb58587e200ad04617f0fab
manuelRojasv <56967748+manuelRojasv@users.noreply.github.com>
2020-09-03 18:25:01 -0400

    Add files via upload
    modify the File in order to add tests just for book and cancel events in Home page due the several UI modifications

---
+++ src/test/java/VivayaTests/BookingCancelEventHomePage.java
@@ -0,0 +1,71 @@
+package VivayaTests;
+
+import Page_Obj.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class BookingCancelEventHomePage {
+    WebDriver driver;
+
+    @BeforeClass
+    @Parameters({"URL", "BrowserType"})
+    public void initial_Setup(String url, String browserType) {
+
+        if (browserType.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            HomePage objHome = new HomePage(driver);
+            LoginPage objLogin = new LoginPage(driver);
+            driver.get(url);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            objHome.click_Login_Link();
+            objLogin.set_Guide_Credentials ("manurex@manu.com", "perrodelmal");
+
+
+        }
+
+        System.out.println("Opening" + browserType);
+    }
+
+    /*@AfterClass
+    public void Teardown() {
+        driver.close();
+        driver.quit();
+    }*/
+
+    @Test(enabled = true, priority = 0)
+    public void book_and_Cancel_From_Home() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        DashboardPage objDash = new DashboardPage(driver);
+        ClassPage classPage = new ClassPage(driver);
+        HomePage objHome = new HomePage(driver);
+        LoginPage objLogin = new LoginPage(driver);
+        objDash.open_Class();
+        classPage.create_Class_Current_Date();
+        Thread.sleep(3000);
+        objDash.logout_From_Dashboard();
+        objHome.click_Login_Link();
+        objLogin.set_Seeker_Credentials("horcosio@gmail.com", "perrodelmal");
+        Thread.sleep(3000);
+        objHome.go_To_Home();
+        objHome.book_First_Upcoming_Event();
+        objHome.go_To_Home();
+        Thread.sleep(3000);
+        objHome.cancel_Booking_from_Upcoming_Event();
+        String sucess = driver.findElement(By.cssSelector("#w0-success-0")).getText();
+        Assert.assertEquals(sucess, "×\n" + "Event has been canceled.");
+        System.out.println(sucess + " From Home by the Seeker");
+    }
+
+}
